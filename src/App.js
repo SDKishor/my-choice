6f56d631de285fed06f5cf9bedc07b65594bde17
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-10 23:26:38 +0600

    all route is set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { About } from "./Pages/About";
+import { Blogs } from "./Pages/Blogs";
+import { Dashboard } from "./Pages/Dashboard";
+import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/NotFound";
+import { Reviews } from "./Pages/Reviews";
 
 function App() {
   return (
     <div className="App">
-      <h1>APP</h1>
+      {/* navigation bar */}
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/">REVIEWS</Link>
+        <Link to="/">DASHBOARD</Link>
+        <Link to="/">BLOGS</Link>
+        <Link to="/">ABOUT</Link>
+      </nav>
+
+      {/* all the routes */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="about" element={<About />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
